cf2bcc7bcb54ed51925ef8da454a5d60495db601
MuriloVantini <m.henrique.vantini18@gmail.com>
2026-05-05 20:34:38 -0300

    feat: adicionar valor padrão no DTO de login para facilitar testes

--- src/main/java/com/agt/viagens/application/dto/request/LoginRequest.java
+++ src/main/java/com/agt/viagens/application/dto/request/LoginRequest.java
@@ -1,8 +1,14 @@
 package com.agt.viagens.application.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-        @NotBlank(message = "Username é obrigatório") String username,
-        @NotBlank(message = "Password é obrigatório") String password
+        @NotBlank(message = "Username é obrigatório")
+        @Schema(example = "usuario.teste")
+        String username,
+
+        @NotBlank(message = "Password é obrigatório")
+        @Schema(example = "senha123")
+        String password
 ) {}
